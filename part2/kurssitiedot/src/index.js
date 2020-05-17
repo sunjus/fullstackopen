16294bf5d20552f89eb71340d8abf87e4a88800a
sunjus <sunju.shin1108@gmail.com>
2020-05-17 15:51:03 +0300

    Solve exercises 2.1

---
+++ part2/kurssitiedot/src/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} key={part.id} />
+      ))}
+    </div>
+  );
+};
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header name={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
