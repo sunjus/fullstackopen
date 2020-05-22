2c32bccfd3b083b80e1c32278b3f0bb353da4cfa
sunjus <sunju.shin1108@gmail.com>
2020-05-22 23:46:52 +0300

    Solve ex 2.5_Separate module

--- part2/kurssitiedot/src/index.js
+++ part2/kurssitiedot/src/index.js
@@ -1,59 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = (props) => {
-  return <h2>{props.course}</h2>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.id}
-      {props.part} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((value) => (
-        <Part part={value.name} exercises={value.exercises} key={value.id} />
-      ))}
-    </div>
-  );
-};
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
-    </div>
-  );
-};
-
-const Courses = (props) => {
-  return (
-    <div>
-      {props.courses.map((value) => (
-        <Course course={value} key={value.name} />
-      ))}
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const total = props.parts.reduce((total, part) => {
-    return total + part.exercises;
-  }, 0);
-  return (
-    <p>
-      <b>total of {total} exercises</b>
-    </p>
-  );
-};
+import Courses from "./components/Courses";
 
 const App = () => {
   const courses = [
