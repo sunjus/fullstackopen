382b2b3f12ed6ce5f5c68432f81dea1f3254ddb4
sunjus <sunju.shin1108@gmail.com>
2021-04-23 15:56:53 +0300

    ex 2.9 Implemented a search field using filter().includes()

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,22 +3,15 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
-  console.log(persons[0]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-
-  const handleNameInput = (e) => {
-    console.log(e.target.value);
-    setNewName(e.target.value);
-  };
-
-  const handleNumberInput = (e) => {
-    console.log(e.target.value);
-    setNewNumber(e.target.value);
-  };
+  const [searchNameExist, setSearchNameExist] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,32 +23,49 @@ const App = () => {
     if (nameExist) {
       return alert(`${newName} is already added to phonebook`);
     }
-
     setPersons([...persons, newPerson]);
     setNewName("");
     setNewNumber("");
   };
 
+  const searchPerson = persons.filter((p) =>
+    p.name.toLowerCase().includes(searchNameExist.toLowerCase())
+  );
+  console.log(searchPerson);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          value={searchNameExist}
+          onChange={(e) => setSearchNameExist(e.target.value)}
+        />
+      </div>
       <form onSubmit={handleSubmit}>
+        <h3>Add a new</h3>
         <div>
-          name: <input value={newName} onChange={handleNameInput} />
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberInput} />
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person, index) => (
-          <div key={index}>
-            {person.name}
-            {person.number}
+        {searchPerson.map((p, i) => (
+          <div key={i}>
+            {p.name}
+            {p.number}
           </div>
         ))}
       </ul>
